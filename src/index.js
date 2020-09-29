91fd42f53ca66416b73327538b55ff527e81795a
yeffry <yeffry45@gmail.com>
2020-09-29 18:39:25 -0400

    refactor code with module pattern

--- src/index.js
+++ src/index.js
@@ -1,56 +1,66 @@
+const weatherData = (() => {
+	const getTemp = async (city) => {
+		const apiKey = '50e876f0306cf9fa7f04e6e896979c45';
 
-const getTemp = async (city) => {
-  const apiKey = '50e876f0306cf9fa7f04e6e896979c45';
+		const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+		const data = await response.json();
+		return data.main.temp;
+	};
 
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-  const data = await response.json();
-  return data.main.temp
-}
+	function getCity(e) {
+		e.preventDefault();
+		return e.target.city.value;
+	}
+})();
 
-function toggleMetric () {
+const weatherUI = (() => {
+	function renderUI(data) {
+		const celsiusTemp = convertKelvinToCelsius(data);
+		const fahrenheitTemp = convertKelvinToFahrenheit(data);
 
-}
+		document.querySelector(
+			'.weather-output-temp'
+		).textContent = `Temperature is ${fahrenheitTemp.temperature} ${fahrenheitTemp.type}`;
+	}
+})();
 
-function getCity (e) {
-  e.preventDefault();
-  return e.target.city.value
-}
+const weatherControl = (() => {
+	function convertKelvinToCelsius(temp) {
+		return {
+			type: 'C°',
+			temperature: Math.round(temp - 273.15)
+		};
+	}
 
-function convertKelvinToCelsius (temp) {
-  return {
-    type: 'C°',
-    temperature: Math.round(temp - 273.15)
+	function convertKelvinToFahrenheit(temp) {
+		return {
+			type: 'F°',
+			temperature: Math.round(temp * 9 / 5 - 459.67)
+		};
   }
-}
-
-function convertKelvinToFahrenheit (temp) {
   
-  return {
-    type: 'F°',
-    temperature: Math.round(temp * 9/5 - 459.67),
-  }
-}
+  document.querySelector('.weather-input-form').addEventListener('submit', (e) => {
+    const city = getCity(e);
+    getTemp(city).then((data) => {
+      renderUI(data);
+    });
+  });
+
+  document.querySelector('.weather-metric').addEventListener('click', (e) => {
+    e.preventDefault();
+    if (e.target.textContent === 'C°') {
+      console.log('cel');
+    } else if (e.target.textContent === 'F°') {
+      console.log('fah');
+    }
+  });
+})();
 
 
-function renderUI (data) {
-  
-  const celsiusTemp = convertKelvinToCelsius(data);
-  const fahrenheitTemp = convertKelvinToFahrenheit(data);
-
-  document.querySelector('.weather-output-temp').textContent = `Temperature is ${fahrenheitTemp.temperature} ${fahrenheitTemp.type}`
-}
 
 
-document.querySelector('.weather-input-form').addEventListener('submit', (e) => {
-  const city = getCity(e)
-  getTemp(city).then((data) => {
-    renderUI(data)
-  })
-})
 
-document.querySelector('.toggle-btn').addEventListener('click', () => {
-  
-})
+
 // For tommorow
 
 // We need a way to make 2 choices, like 2 buttons or 2 radios
